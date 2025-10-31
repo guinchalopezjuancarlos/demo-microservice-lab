f5968a860cefddecad5458d294e37d4d8c62bde7
guinchalopezjuancarlos <117472300+guinchalopezjuancarlos@users.noreply.github.com>
2025-10-31 03:51:20 -0400

    Primer commit

--- infra/main.tf
+++ infra/main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "this" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.admin_cidr_ssh]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
