0680264aee7481107a1f37b7ed134a8446ae2d76
guinchalopezjuancarlos <117472300+guinchalopezjuancarlos@users.noreply.github.com>
2025-10-31 12:38:06 -0400

    Node.js escucha en 0.0.0.0 para acceso público

--- app/server.js
+++ app/server.js
@@ -2,10 +2,10 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
-  console.log(` Servidor corriendo en http://localhost:${PORT}`);
+const server = app.listen(PORT, "0.0.0.0", () => {  
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
 
-// Cerrar server correctamente con SIGTERM/SIGINT
+
 process.on("SIGTERM", () => server.close(() => process.exit(0)));
 process.on("SIGINT", () => server.close(() => process.exit(0)));
